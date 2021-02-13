28bf065a697ee1bac76bd351b1f401067ed2b0c7
qjordnvb <jordanvillarreal1994@gmail.com>
2021-02-13 08:59:41 -0500

    WIP

---
+++ src/components/search/search.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./search.css";
+import SearchInput from "./search-input";
+import SearchDropdown from "./search-dropdown";
+
+const Search = () => (
+  <section>
+    <div className="container">
+      <SearchInput />
+      <SearchDropdown />
+    </div>
+  </section>
+);
+
+export default Search;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/header/header";
+import Search from "../../components/search/search";
+import List from "../../components/list/list";
+
+const Home = () => (
+  <>
+    <Header />
+    <Search />
+    <List />
+  </>
+);
+
+export default Home;
